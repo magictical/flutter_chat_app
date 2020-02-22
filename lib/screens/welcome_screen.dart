04b63f4053bcd6b3f1e90050105c294bbb1d17d3
IL KWON KIM <magictical@gmail.com>
2020-02-23 08:27:22 +0900

    add navigator to each button when it pressed

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,3 +1,5 @@
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatefulWidget {
@@ -44,6 +46,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 child: MaterialButton(
                   onPressed: () {
                     //Go to login screen.
+                    Navigator.of(context).pushNamed(LoginScreen.routeName);
                   },
                   minWidth: 200.0,
                   height: 42.0,
@@ -62,6 +65,8 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 child: MaterialButton(
                   onPressed: () {
                     //Go to registration screen.
+                    Navigator.of(context)
+                        .pushNamed(RegistrationScreen.routeName);
                   },
                   minWidth: 200.0,
                   height: 42.0,
